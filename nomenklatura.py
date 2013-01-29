9bda146ef81df25944c4264ecf836c041749bce2
Michael Bauer <mihi@lo-res.org>
2013-01-29 09:53:39 +0100

    exceptions are now part of the class (no need for seperate importing to except)
    Further values and links now return generators - these tend to be more
    memory efficient...

--- nomenklatura.py
+++ nomenklatura.py
@@ -3,178 +3,218 @@ import json
 
 
 class NKObject(object):
- 
- def __init__(self,data):
-  for k,v in data.items():
-    setattr(self,k,v)
+  """ The basic Object we'll be inheriting from """
 
-def apply_attrs(obj, data):
-    for k, v in data.items():
-        setattr(obj, k, v)
+  def __init__(self,data):
+    for k,v in data.items():
+      setattr(self,k,v) 
+      # TODO - beautify this with accessor methods (__getattribute__...)
 
-
-class NKException(Exception):
+  class NKException(Exception):
 
     def __init__(self, data):
-        apply_attrs(self, data)
-
-
-class NKDatasetException(NKException):
+      for k, v in data.items():
+        setattr(self, k, v)
+        # TODO - same here - beautify
 
     def __repr__(self):
-        return "<NKDatasetException(%s:%s)>" % (self,
+      return "<NKException>"
+
+    def __str__(self):
+      return self.__repr__()
+
+
+  class DatasetException(NKException):
+
+    def __repr__(self):
+        return "<DatasetException(%s:%s)>" % (self,
                                                getattr(self, 'message', None))
 
 
-class NKNoMatch(NKException):
+  class NoMatch(NKException):
 
     def __repr__(self):
-        return "<NKNoMatch(%s:%s)>" % (self.dataset,
+      return "<NoMatch(%s:%s)>" % (self.dataset,
                                        self.key)
 
 
-class NKInvalid(NKException):
+  class Invalid(NKException):
 
     def __repr__(self):
-        return "<NKInvalid(%s:%s)>" % (self.dataset,
-                                       self.key)
+      return "<Invalid(%s:%s)>" % (self.dataset,
+                                     self.key)
 
 
 class Value(NKObject):
 
-    def __init__(self, dataset, data):
-        self._dataset = dataset
-        apply_attrs(self, data)
+  def __init__(self, dataset, data):
+    self._dataset = dataset
+    super(Value,self).__init__(data)
 
-    def __repr__(self):
-        return "<Value(%s:%s:%s)>" % (self._dataset.name,
-                                        self.id, self.value)
+  def __repr__(self):
+    return "<Value(%s:%s:%s)>" % (self._dataset.name,
+                                      self.id, self.value)
 
 
 class Link(NKObject):
 
-    INVALID = "INVALID"
-    NEW = "NEW"
+  INVALID = "INVALID"
+  NEW = "NEW"
 
-    def __init__(self, dataset, data):
-        self._dataset = dataset
-        super(NKLink,self).__init__(data)
+  def __init__(self, dataset, data):
+      self._dataset = dataset
+      super(Link,self).__init__(data)
 
-    def __repr__(self):
-        return "<Link(%s:%s:%s:%s)>" % (self._dataset.name,
-                                       self.id, self.key, self.is_matched)
+  def __repr__(self):
+      return "<Link(%s:%s:%s:%s)>" % (self._dataset.name,
+                                     self.id, self.key, self.is_matched)
 
 
-class Dataset(object):
+class Dataset(NKObject):
+  """ A Nomenklatura dataset. Helps you to access values and links from
+  Nomenklatura...
+  
+  Dataset(name,host="http://nomenklatura.okfnlabs.org",api_key=None)
 
-    def __init__(self, dataset,
-                 host='http://nomenklatura.okfnlabs.org',
-                 api_key=None):
-        self.host = host
-        self.name = dataset
-        self.api_key = api_key
-        self._fetch()
+  usage:
+    
+    from nomenklatura import dataset
+    ds=Dataset("offenesparlament")
+    ds.lookup("Angela Merkel")
 
-    @property
-    def _session(self):
-        if not hasattr(self, '_session_obj'):
-            headers = {'Accept': 'application/json',
-                       'Content-Type': 'application/json'}
-            if self.api_key:
-                headers['Authorization'] = self.api_key
-            self._session_obj = requests.Session()
-            self._session_obj.headers.update(headers)
-        return self._session_obj
+  Methods defined here:
 
-    def _get(self, path, params={}, retry=True):
-        response = self._session.get(self.host + '/' + self.name + path,
-                                     params=params)
-        if not response.ok:
-            #print [response.status_code, response.content]
-            del self._session_obj
-        return response.status_code, response.json()
+  get_value(id=None,value=None) """
 
-    def _post(self, path, data={}, retry=True):
-        data = json.dumps(data)
-        response = self._session.post(self.host + '/' + self.name + path,
-                                      allow_redirects=True,
-                                      data=data)
-        if not response.ok:
-            #print [response.status_code, response.content]
-            del self._session_obj
-        return (response.status_code,
-                json.loads(response.content) if response.content else {})
 
-    def _fetch(self):
-        code, data = self._get('')
-        if not (code == 200 and data):
-            data = data or {'code': code}
-            raise NKDatasetException(data)
-        apply_attrs(self, data)
 
-    def get_value(self, id=None, value=None):
-        assert id or value, "Need to give an ID or a value!"
-        if id is not None:
-            code, val = self._get('/values/%s' % id)
-        else:
-            code, val = self._get('/value', params={'value': value})
-        if code != 200:
-            raise NKException(val or {})
-        return Value(self, val)
+  def __init__(self, dataset,
+               host='http://nomenklatura.okfnlabs.org',
+               api_key=None):
+      self.host = host
+      self.name = dataset
+      self.api_key = api_key
+      self._fetch()
 
-    def add_value(self, value, data={}):
-        code, val = self._post('/values',
-                               data={'value': value, 'data': data})
-        if code == 400:
-            raise NKException(val)
-        return Value(self, val)
+  @property
+  def _session(self):
+      if not hasattr(self, '_session_obj'):
+          headers = {'Accept': 'application/json',
+                     'Content-Type': 'application/json'}
+          if self.api_key:
+              headers['Authorization'] = self.api_key
+          self._session_obj = requests.Session()
+          self._session_obj.headers.update(headers)
+      return self._session_obj
 
-    def ensure_value(self, value, data={}):
-        try:
-            return self.get_value(value=value)
-        except NKException:
-            return self.add_value(value=value, data=data)
+  def _get(self, path, params={}, retry=True):
+      response = self._session.get(self.host + '/' + self.name + path,
+                                   params=params)
+      if not response.ok:
+          #print [response.status_code, response.content]
+          del self._session_obj
+      return response.status_code, response.json()
 
-    def values(self):
-        code, vals = self._get('/values')
-        return [Value(self, v) for v in vals]
+  def _post(self, path, data={}, retry=True):
+      data = json.dumps(data)
+      response = self._session.post(self.host + '/' + self.name + path,
+                                    allow_redirects=True,
+                                    data=data)
+      if not response.ok:
+          #print [response.status_code, response.content]
+          del self._session_obj
+      return (response.status_code,
+              json.loads(response.content) if response.content else {})
 
-    def get_link(self, id=None, key=None):
-        assert id or key, "Need to give an ID or a key!"
-        if id is not None:
-            code, val = self._get('/links/%s' % id)
-        else:
-            code, val = self._get('/link', params={'key': key})
-        if code != 200:
-            raise NKException(val)
-        return Link(self, val)
+  def _fetch(self):
+      code, data = self._get('')
+      if not (code == 200 and data):
+          data = data or {'code': code}
+          raise self.DatasetException(data)
+      super(Dataset,self).__init__(data)    
 
-    def links(self):
-        code, vals = self._get('/links')
-        return [Link(self, v) for v in vals]
+  def get_value(self, id=None, value=None):
+    """ get a value from the dataset 
+        get_value(id=23) -> get value with id 23
+        get_value(value="FOO") -> get value with value "FOO"
 
-    def lookup(self, key, context={}, readonly=False):
-        code, val = self._post('/lookup',
-                               data={'key': key,
-                                     'readonly': readonly})
-        if code == 404:
-            raise NKNoMatch(val)
-        elif code == 418:
-            raise NKInvalid(val)
-        else:
-            return Value(self, val.get('value'))
+        if neither id nor value are specified - raises a Value
+        Error
+    """
 
-    def match(self, link_id, value_id):
-        code, val = self._post('/links/%s/match' % link_id,
-                               data={'choice': value_id,
-                                     'value': ''})
-        if code != 200:
-            raise NKException(val)
-        return None
+    if not (id or value):
+      raise ValueError("Need to give an ID or a value")
+    if id is not None:
+        code, val = self._get('/values/%s' % id)
+    else:
+        code, val = self._get('/value', params={'value': value})
+    if code != 200:
+        raise self.NKException(val or {})
+    return Value(self, val)
 
-    def __repr__(self):
-        return "<Dataset(%s)>" % self.name
+  def add_value(self, value, data={}):
+    """ Add a value to the dataset """
+    code, val = self._post('/values',
+                           data={'value': value, 'data': data})
+    if code == 400:
+        raise self.NKException(val)
+    return Value(self, val)
 
+  def ensure_value(self, value, data={}):
+    """ Makes sure you have a value to work with:
+      ensure_value(value,data) ->
+        Returns a Value if it exists - adds the value otherwise
+        """
+    try:
+        return self.get_value(value=value)
+    except self.NKException:
+        return self.add_value(value=value, data=data)
+
+  def values(self):
+    """ Returns a generator of all Values in the dataset """
+    code, vals = self._get('/values')
+    return (Value(self, v) for v in vals) 
+
+  def get_link(self, id=None, key=None):
+      if not (id or key):
+        raise ValueError("Need to give an ID or a Key")
+      if id:
+          code, val = self._get('/links/%s' % id)
+      else:
+          code, val = self._get('/link', params={'key': key})
+      if code != 200:
+          raise self.NKException(val)
+      return Link(self, val)
+
+  def links(self):
+    """ Returns a generator of all links in the dataset """
+    code, vals = self._get('/links')
+    return (Link(self, v) for v in vals)
+
+  def lookup(self, key, context={}, readonly=False):
+      code, val = self._post('/lookup',
+                             data={'key': key,
+                                   'readonly': readonly})
+      if code == 404:
+          raise self.NoMatch(val)
+      elif code == 418:
+          raise self.Invalid(val)
+      else:
+          return Value(self, val.get('value'))
+
+  def match(self, link_id, value_id):
+      code, val = self._post('/links/%s/match' % link_id,
+                             data={'choice': value_id,
+                                   'value': ''})
+      if code != 200:
+          raise self.NKException(val)
+      return None
+
+  def __repr__(self):
+      return "<Dataset(%s)>" % self.name
+
+    def __str__(self):
+      return self.__repr__()
 
 if __name__ == "__main__":
-    ds = Dataset('offenesparlament', 'http://localhost:5000')
+  ds = Dataset('offenesparlament')
